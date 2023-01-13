91091115cf36b33151adc6fdefa68dc92f79c693
Marko Koschak <marko.koschak@tisno.de>
2023-01-13 17:55:07 +0100

    Use kproject to load Fleks ECS + update readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,9 @@ plugins {
 }
 
 korge {
-	id = "com.sample.demo"
-
-	// Using Fleks from maven central / local
-	dependencyMulti("io.github.quillraven.fleks:Fleks:2.0", registerPlugin = false)
+	id = "com.sample.korgeflekshelloworld"
 
 // To enable all targets at once
-
 	targetAll()
 
 // To enable targets based on properties/environment variables
@@ -23,6 +19,11 @@ korge {
 	targetJs()
 	targetDesktop()
 	targetIos()
-	targetAndroidIndirect() // targetAndroidDirect()
-	//targetAndroidDirect()
+	targetAndroidIndirect()
+	serializationJson()
+    //targetAndroidDirect()
+}
+
+dependencies {
+	add("commonMainApi", project(":deps"))
 }
