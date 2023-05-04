e0a3c0d8fc786feee9364ce907519d3f29b23765
soywiz <soywiz@gmail.com>
2023-05-04 23:15:26 +0200

    Update to KorGE 4.0.0-rc4 + kproject 0.2.1 + simplify dependencies

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -10,48 +10,41 @@ import korlibs.math.interpolation.*
 import com.github.quillraven.fleks.*
 import com.github.quillraven.fleks.World.Companion.family
 
-suspend fun main() {
-	 return Korge(
-		 config = KorgeConfig(
-			 virtualSize = Size(width = 512, height = 512),
-			 backgroundColor = Colors["#2b2b2b"]
-		 )
-	 ) {
-		 val minDegrees = (-16).degrees
-		 val maxDegrees = (+16).degrees
+suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors["#2b2b2b"]) {
+	val minDegrees = (-16).degrees
+	val maxDegrees = (+16).degrees
 
-		 val image = image(resourcesVfs["korge.png"].readBitmap()) {
-			 rotation = maxDegrees
-			 anchor(.5, .5)
-			 scale(.8)
-			 position(256, 256)
-		 }
+	val image = image(resourcesVfs["korge.png"].readBitmap()) {
+		rotation = maxDegrees
+		anchor(.5, .5)
+		scale(.8)
+		position(256, 256)
+	}
 
-		 val world = world {
-			 // Register all needed systems of the entity component system
-			 // The order of systems here also define the order in which the systems are called inside Fleks ECS
-			 systems {
-				 add(HelloWorldSystem())
-			 }
-		 }
+	val world = world {
+		// Register all needed systems of the entity component system
+		// The order of systems here also define the order in which the systems are called inside Fleks ECS
+		systems {
+			add(HelloWorldSystem())
+		}
+	}
 
-		 // Run the update of the Fleks ECS - this will periodically call all update functions of the systems (e.g. onTick(), onTickEntity(), etc.)
-		 addUpdater { dt ->
-			 world.update(dt.seconds.toFloat())
-		 }
+	// Run the update of the Fleks ECS - this will periodically call all update functions of the systems (e.g. onTick(), onTickEntity(), etc.)
+	addUpdater { dt ->
+		world.update(dt.seconds.toFloat())
+	}
 
-		 // Create entity for printing out "Hello World!"
-		 world.entity {
-			 it += HelloWorldComponent(
-				 text = "Hello World!"
-			 )
-		 }
+	// Create entity for printing out "Hello World!"
+	world.entity {
+		it += HelloWorldComponent(
+			text = "Hello World!"
+		)
+	}
 
-		 while (true) {
-			 image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-			 image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-		 }
-	 }
+	while (true) {
+		image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
+		image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
+	}
 }
 
 // Component to store "Hello World!" string for printing
