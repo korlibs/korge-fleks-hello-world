c460c9db623c9886574e06d8fafcc6c77eb06370
Carlos Ballesteros Velasco <soywiz@gmail.com>
2023-05-15 11:45:08 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement { repositories { mavenLocal(); mavenCentral(); google(); gradlePluginPortal() } }
 
 plugins {
-    id("com.soywiz.kproject.settings") version "0.2.1"
+    id("com.soywiz.kproject.settings") version "0.2.7"
     //id("com.soywiz.kproject.settings") version "0.0.1-SNAPSHOT"
 }
 
